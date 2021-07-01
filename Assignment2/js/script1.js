e412eb5dadfd46b517100f1e0e7ac8b1b29c4c7d
Ayan Agrawal <82317309+AyanAgrawal25@users.noreply.github.com>
2021-07-01 11:41:19 +0530

    Add files via upload

---
+++ Assignment2/js/script1.js
@@ -0,0 +1,26 @@
+// NAVBAR FIXED
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.toggle("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.addEventListener("scroll", function(){
+  var header = document.querySelector(".bar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
